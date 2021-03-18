b09b12f56bac855ba4213e13800a89ba09fd9bbb
Michele Tobias <mmtobias@ucdavis.edu>
2021-03-18 15:34:14 -0700

    Create WorkshopScript.R

---
+++ RCode/WorkshopScript.R
@@ -0,0 +1,41 @@
+#OBJECTIVE: demonstrate common tasks in R related to projections.
+#Author: Michele Tobias, PhD
+
+#workshop materials: https://github.com/MicheleTobias/R-Projections-Workshop
+
+
+
+# Set Up ----------------------------------------------------------
+
+# load libraries
+
+# set working directory
+
+# load data
+
+
+
+# Vector Data CRS ---------------------------------------------------------
+
+# identify the existing CRS
+
+# define a CRS when it is missing
+
+# tranform/reproject vector data
+
+# plot the data
+
+
+
+# Raster Data CRS ---------------------------------------------------------
+
+# identify the existing CRS
+
+# define a CRS when it is missing
+
+# tranform/reproject vector data
+
+# plot the data
+
+
+
